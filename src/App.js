84e3fd7e3dec943bef25328402a6c9a09bdc8804
ADITI DWIVEDI <addoo96@gmail.com>
2020-09-06 15:51:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import UpcomingEvents from "./Components/UpcomingEvents";
+// import TableExample from "./Components/TableExample";
+
+function App() {
+  return (
+    <div className="App">
+      <UpcomingEvents />
+      {/* <TableExample /> */}
+    </div>
+  );
+}
+
+export default App;
